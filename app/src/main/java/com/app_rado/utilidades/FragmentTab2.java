aabc359d1d4f70f633f4a983169e1e142625a60b
arlysanchez <jhansistema@gmail.com>
2015-06-03 13:40:26 -0500

    segundo commit

--- app/src/main/java/com/app_rado/utilidades/FragmentTab2.java
+++ app/src/main/java/com/app_rado/utilidades/FragmentTab2.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.app_rado.R;
 
 public class FragmentTab2 extends Fragment {
